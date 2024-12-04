4f9b02b263a6171fefadb9bef2860ba24c4f41eb
JayGhz <jaykansas7@gmail.com>
2024-12-03 22:26:45 -0500

    feat: Implement HomePage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,29 @@
+import HeaderBox from '@/components/header-box'
+import TotalBalanceBox from '@/components/total-balance-box'
 import React from 'react'
 
-function Home() {
+const Home = () => {
+  const loggedIn = { firstName: 'Jay' }
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type='greeting'
+            title='Welcome'
+            user={loggedIn?.firstName || 'Guest'}
+            subtext='Access and manage your account and transactions efficiently.'
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.32}
+          />
+
+        </header>
+      </div>
+    </section>
   )
 }
 
