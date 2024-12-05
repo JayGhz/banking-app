20e571729318c208df1ed8a24169159e61cd3557
JayGhz <jaykansas7@gmail.com>
2024-12-04 22:00:07 -0500

    feat: Add RightSidebar component and integrate user details and bank information in Home

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import HeaderBox from '@/components/header-box'
+import RightSidebar from '@/components/right-sidebar'
 import TotalBalanceBox from '@/components/total-balance-box'
 import React from 'react'
 
 const Home = () => {
-  const loggedIn = { firstName: 'Jay' }
+  const loggedIn = { firstName: 'Jay', lastName: 'GHZ', email: 'contact@gmail.com' }
   return (
     <section className='home'>
       <div className='home-content'>
@@ -20,9 +21,15 @@ const Home = () => {
             totalBanks={1}
             totalCurrentBalance={1250.32}
           />
-
         </header>
+
+        Recent Transactions
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 1140.50 }, { currentBalance: 110.82 }]}
+      />
     </section>
   )
 }
