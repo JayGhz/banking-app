f7860b5798e4a47eabf2d6026199343bbd187f24
JayGhz <jaykansas7@gmail.com>
2024-12-05 00:26:45 -0500

    feat: Implement custom input components and integrate form validation for sign-up and sign-in

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import AuthForm from '@/components/auth-form'
 
-const SigIn = () => {
+const SignIn = () => {
   return (
-    <div>SigIn</div>
+    <section className="flex-center size-full max-sm:px-6">
+      <AuthForm type="sign-in" />
+    </section>
   )
 }
 
-export default SigIn
+export default SignIn
