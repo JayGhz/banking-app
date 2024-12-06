c219720769e7b7bd31269dfde3484670c014b899
JayGhz <jaykansas7@gmail.com>
2024-12-05 20:44:11 -0500

    feat: Integrate Sentry for error monitoring and add example API route for testing

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://9a02f4f7d7b93c12012865ec1eb7fe7a@o4508418605056000.ingest.us.sentry.io/4508418608136192",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
